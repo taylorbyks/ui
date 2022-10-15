fee7dfe9d8925412ff89096fd59b88ac0c6c0160
Taylor Schinvelski <taylorbyks@hotmail.com>
2022-10-15 15:24:12 -0300

    feat: first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    fontSize: {
+      sm: 14,
+      md: 16,
+      lg: 18,
+      xl: 20,
+      '2xl': 24,
+      '3xl': 32,
+    },
+    colors: {
+      transparent: 'transparent',
+      black: '#000000',
+      white: '#FFFFFF',
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7C7C8A',
+        200: '#C4C4CC',
+        100: '#E1E1E6',
+      },
+      cyan: {
+        800: '#12B4EE',
+        500: '#81D8F7',
+        300: '#A7E2F6',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Work Sans, sans-serif',
+      },
+    },
+  },
+  plugins: [],
+}
