119083a1b29bb0a2a46b1b2722e0dc1aba8cdd45
Taylor Schinvelski <taylorbyks@hotmail.com>
2022-11-20 00:25:46 -0300

    chore: add config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  jit: true,
   content: ['./src/**/*.tsx'],
   theme: {
     fontSize: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,24 @@
 import react from '@vitejs/plugin-react'
+import plugin from '@vitejs/plugin-react'
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   plugins: [
-    react(),
+    plugin({
+      'jsxRuntime': 'classic'
+    }),
+    // react(),
     dts({
       insertTypesEntry: true,
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
