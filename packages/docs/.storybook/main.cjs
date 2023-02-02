2c66ffdbdbec14d8f5d42c57da32928e84e4c037
Taylor Schinvelski <taylorbyks@hotmail.com>
2023-02-02 18:19:46 -0300

    refactor: segregate

--- packages/docs/.storybook/main.cjs
+++ packages/docs/.storybook/main.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../../**/src/**/*.stories.mdx', '../../**/src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-a11y'],
   framework: '@storybook/react',
   core: {
